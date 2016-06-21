99aba6075d7ba5a3baa48dd64053ea3c2ddc6db5
chenly <chenly@chenlyPC>
2016-06-21 09:39:43 +0800

    pascloud service base

---
+++ pas-cloud-parent/pascloud1.sql
@@ -0,0 +1,23 @@
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: pascloud1
+Target Host: localhost
+Target Database: pascloud1
+Date: 2016/6/20 18:09:29
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+CREATE TABLE `t_user` (
+  `userId` int(11) DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- ----------------------------
+-- Records 
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'ccc', '3rerw3');
